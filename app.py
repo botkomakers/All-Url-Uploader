e53e5a9e33410ff2da438d02944ab9421ca2b6e1
botkomakers <siam420riiya@gmail.com>
2025-05-11 22:06:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-@app.route("/")
-def hello_world():
-    return "Hello from Koyeb"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # Run the Flask app on port 80
+    app.run(host='0.0.0.0', port=80)
